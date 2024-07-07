9c0f62b507bcde52d8f1426031f00ca56412b8ec
Egwu15 <egwutedd@gmail.com>
2024-07-07 19:56:02 +0100

    remove fillament install

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Livewire\Blog;
 use App\Livewire\BlogSingle;
 use Illuminate\Support\Facades\Route;
 
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-
 Route::view('/', 'welcome')->name('home');
 
 Route::view('profile', 'profile')
@@ -20,18 +17,4 @@ Route::get('blog',  Blog::class)->name('blog');
 Route::get('blog/{id}', BlogSingle::class)->name('single-post');
 
 
-
-Route::get('/install-filament', function () {
-    // Security checks should be here
-
-    $process = new Process(['composer', 'require', 'filament/filament']);
-    $process->setWorkingDirectory(base_path());
-    try {
-        $process->mustRun();
-
-        echo 'Filament installed successfully';
-    } catch (ProcessFailedException $exception) {
-        echo 'Filament installation failed: ' . $exception->getMessage();
-    }
-}); 
 // require __DIR__ . '/auth.php';
