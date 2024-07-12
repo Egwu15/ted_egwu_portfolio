33c5694861ba5bdb28100cb8e14eebed7ea7a6ef
Egwu15 <egwutedd@gmail.com>
2024-07-12 01:29:18 +0100

    refactored services and added content to home and other pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,56 +149,7 @@
     <!--================ End Brand Area =================-->
 
     <!--================ Start Features Area =================-->
-    <section class="features_area">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="text-center col-lg-8">
-                    <div class="main_title">
-                        <h2>service offers </h2>
-                        <p>
-                            Maximizing Business Efficiency Through Exceptional Development Practices
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row feature_inner">
-                <div class="col-lg-3 col-md-6">
-                    <div class="feature_item">
-                        <img src="img/services/s1.png" alt="">
-                        <h4>Web </h4>
-                        <p>Specializing in responsive website design and development, tailored to enhance your online
-                            presence and drive conversions.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="feature_item">
-                        <img src="img/services/s2.png" alt="">
-                        <h4>Mobile </h4>
-                        <p>Developing impactful mobile apps for iOS and Android, ensuring exceptional user experiences
-                            and business success.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="feature_item">
-                        <img src="img/services/s3.png" alt="">
-                        <h4>Backend </h4>
-                        <p>Building scalable, secure server-side solutions, including database management, to optimize
-                            applications.</p>
-
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="feature_item">
-                        <img src="img/services/s4.png" alt="">
-                        <h4>Team leadership</h4>
-                        <p>Empowering leadership through tailored coaching and mentorship programs, designed to
-                            inspire and guide teams to success.</p>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('components.services')
     <!--================ End Features Area =================-->
 
 
